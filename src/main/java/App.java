a04bb89a0553eba6a96f812b6863bd58d33812cc
Alexey Ananichev <Ananichev@internet.ru>
2021-01-09 17:42:45 +0300

    complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Predicate;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat= (Cat) applicationContext.getBean("cat");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean1);
+        System.out.println(beanCat == beanCat1);
+
     }
 }
